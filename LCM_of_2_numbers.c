bef0bbb1c866001f56a9b661f8c75e676e300d72
Yashsb28 <78728820+Yashsb28@users.noreply.github.com>
2021-02-25 02:46:38 +0530

    Update LCM_of_2_numbers.c

--- LCM_of_2_numbers.c
+++ LCM_of_2_numbers.c
@@ -1,39 +1,13 @@
-#include <stdio.h>
- 
-int lcm(int a, int b);
+#include<stdio.h>
 
-
-int main()
+main()
 {
-    int num1, num2, LCM;
- 
-    printf("Enter first number: ");
-    scanf("%d",&num1);
-     printf("Enter second number: ");
-    scanf("%d",&num2);
-     
-     
-    if(num1 > num2)
-        LCM = lcm(num2, num1);
-    else
-        LCM = lcm(num1, num2);
-        
-    printf("LCM of %d and %d = %d", num1, num2, LCM);
-    
-    return 0;
+	int lcm(int a, int b);
 }
-int lcm(int a, int b)
+	lcm()
 {
-    static int multiple = 0;
-
-    multiple += b;
-    
-    if((multiple % a == 0) && (multiple % b == 0))
-    {
-        return multiple;
-    }
-    else 
-    {
-        return lcm(a, b);
-    }
+	int a=0,b=0;
+	printf("Enter Two Numbers : ");
+	scanf("%d%d", &a,&b);
+	printf("\nThe LCM of %d and %d is %d", a,b,lcm);
 }
